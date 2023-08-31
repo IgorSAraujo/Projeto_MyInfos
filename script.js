abfbc2782e567a745d8df4c6de7197378453e4cb
Igor Silva <firetuskbr1@gmail.com>
2023-08-31 13:16:15 -0300

    Função Java para mudar foto e a legenda

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ function toggleMode() {
   } else {
     html.classList.add("light")
   }
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./IMG/Perfil_Global_Oculos.png")
+  } else {
+    img.setAttribute("src", "./IMG/Perfil Global.png")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute(
+      "alt",
+      "Foto de Igor Silva usando camisa preta, oculos escuros e fundo iluminado por led Azul"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "Foto de Igor Silva usando camisa preta, fundo iluminado por led Azul"
+    )
+  }
 }
 
 // a Função "html.classlist.toggle("light")"
